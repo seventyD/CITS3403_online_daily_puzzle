b1105b80881b0800e76fee7014e7849c433c6dc8
IsaacM69 <22701047@student.uwa.edu.au>
2022-05-03 20:48:00 +0800

    Added javascript and stylesheet

---
+++ app/static/javascrpit/java.js
@@ -0,0 +1,23 @@
+function open_help()
+{
+    const help_window = document.getElementById("help_popup");
+    help_window.style.display = "block";  
+}
+
+function close_help()
+{
+    const help_window = document.getElementById("help_popup");
+    help_window.style.display = "none";
+}
+
+function open_stats()
+{
+    const help_window = document.getElementById("stats_popup");
+    help_window.style.display = "block";  
+}
+
+function close_stats()
+{
+    const help_window = document.getElementById("stats_popup");
+    help_window.style.display = "none";
+}
