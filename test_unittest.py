59fc5d754707d0e2151cfacae0bfe259557ad26b
IsaacM69 <22701047@student.uwa.edu.au>
2022-05-22 16:20:29 +0800

    tests work with data in database

--- test_unittest.py
+++ test_unittest.py
@@ -7,12 +7,7 @@ from app.routes import check_day,new_day,init_save
 from app import app, db
 
 import unittest  
-#Some tests may only work if their is nothing already in the database
 class Test_TestModels(unittest.TestCase):
-
-
-        
-
             
     def test_user(self):
         #Tests adding user to database words
@@ -29,28 +24,30 @@ class Test_TestModels(unittest.TestCase):
     def test_goal_words(self):
         #Tests that a new day will create goal words from the correct lists
         new_day()
-        self.assertTrue(Goal_words.query.all()[0].asia in list(open('word_data/ASIA.csv')))
-        self.assertTrue(Goal_words.query.all()[0].australia in list(open('word_data/AUSTRALIA.csv')))
-        self.assertTrue(Goal_words.query.all()[0].europe in list(open('word_data/EUROPE.csv')))
-        self.assertTrue(Goal_words.query.all()[0].north_america in list(open('word_data/NORTH_AMERICA.csv')))
-        self.assertTrue(Goal_words.query.all()[0].south_america in list(open('word_data/SOUTH_AMERICA.csv')))
-        self.assertTrue(Goal_words.query.all()[0].africa in list(open('word_data/AFRICA.csv')))
-        self.assertEqual(Goal_words.query.all()[0].date,str(date.today()))
+        self.assertTrue(Goal_words.query.all()[len(Goal_words.query.all()) - 1].asia in list(open('word_data/ASIA.csv')))
+        self.assertTrue(Goal_words.query.all()[len(Goal_words.query.all()) - 1].australia in list(open('word_data/AUSTRALIA.csv')))
+        self.assertTrue(Goal_words.query.all()[len(Goal_words.query.all()) - 1].europe in list(open('word_data/EUROPE.csv')))
+        self.assertTrue(Goal_words.query.all()[len(Goal_words.query.all()) - 1].north_america in list(open('word_data/NORTH_AMERICA.csv')))
+        self.assertTrue(Goal_words.query.all()[len(Goal_words.query.all()) - 1].south_america in list(open('word_data/SOUTH_AMERICA.csv')))
+        self.assertTrue(Goal_words.query.all()[len(Goal_words.query.all()) - 1].africa in list(open('word_data/AFRICA.csv')))
+        self.assertEqual(Goal_words.query.all()[len(Goal_words.query.all()) - 1].date,str(date.today()))
         self.assertTrue(check_day())
         
         words = Goal_words.query.all()
         for k in words:
-            db.session.delete(k)
+            final = k
+        db.session.delete(final)
         db.session.commit()
+        print(Goal_words.query.all())
 
 
     def test_game(self):
-        #Tests adding game to database words
+        #Tests adding game to database works
         game = Game(win = 0)
         db.session.add(game)
         db.session.commit()
 
-        self.assertEqual(Game.query.all()[0].win,0)
+        self.assertEqual(Game.query.all()[len(Game.query.all()) - 1].win,0)
         db.session.delete(game)
         db.session.commit()
         
@@ -64,17 +61,16 @@ class Test_TestModels(unittest.TestCase):
         db.session.commit()
 
         init_save(user)
-        self.assertEqual(Save.query.all()[0].user_id,User.query.filter_by(username = "test").first().id)
+        self.assertEqual(Save.query.all()[len(Save.query.all()) - 1].user_id,User.query.filter_by(username = "test").first().id)
 
         words = Save.query.all()
         for k in words:
-            db.session.delete(k)
-
+            final = k
+        
+        db.session.delete(final)
         db.session.commit()
         db.session.delete(user)
         db.session.commit()
     
-
-        
 if __name__ == '__main__':
     unittest.main()
