75477fd70f3aabf14734b18e332eb5d48368d2cd
IsaacM69 <22701047@student.uwa.edu.au>
2022-05-22 12:31:38 +0800

    first test commit

---
+++ test_unittest.py
@@ -0,0 +1,11 @@
+from app.models import Save    # The code to test
+import unittest   # The test framework
+from app.forms import RegistrationForm
+
+class Test_TestRoutes(unittest.TestCase):
+    def first_test(self):
+ #       self.assertEqual(validate_username("isaac_muia@hotmail.com")) 
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
